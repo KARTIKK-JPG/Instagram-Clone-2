7cf200cedfead7b9a0d65328e41437d283f32cc8
Kartik Fulara <kartikfulara786@gmail.com>
2025-07-26 17:16:22 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,8 @@ app.use(cookieParser())
 app.use(urlencoded({extended:true}))
 
 const corsOptions = {
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173',"http://localhost:5174",
+    "http://localhost:5175"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   };
